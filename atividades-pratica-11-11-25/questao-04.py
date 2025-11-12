688eb1530e2ce0eaa36ae72696f9f1c01ea6fda1
Jessica Vylia <120236776+vianassej@users.noreply.github.com>
2025-11-12 11:32:15 -0300

    Atividade 04 - Calculadora de preço total

---
+++ atividades-pratica-11-11-25/questao-04.py
@@ -0,0 +1,34 @@
+"""
+4- Calculadora de Preço Total
+* Desenvolva um programa que calcula o preço total de uma compra. Use as seguintes informações:
+
+* Nome do produto: "Cadeira Infantil"
+* Preço unitário: R$ 12.40
+* Quantidade: 3
+O programa deve calcular o preço total e exibir todas as informações, incluindo o resultado final.
+"""
+
+produto = "Cadeira Infantil"
+preco_unitario = 12.40
+quantidade = 3
+
+print("O preço total da compra é R$ {:.2f}".format(preco_unitario * quantidade))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#╔═══════════════════════════╗
+#║       </> vianassej       ║
+#╚═══════════════════════════╝
