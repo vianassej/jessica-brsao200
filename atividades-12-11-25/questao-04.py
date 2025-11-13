ecd61b0882201c8d1fce9aebfb2d6d68fd0bf030
Jessica Vylia <120236776+vianassej@users.noreply.github.com>
2025-11-12 21:13:57 -0300

    Questão 04

---
+++ atividades-12-11-25/questao-04.py
@@ -0,0 +1,42 @@
+"""
+4- Calculadora de Consumo de Combustível
+Desenvolva um programa que calcula o consumo médio de combustível de um veículo.
+Use os seguintes dados:
+
+* Distância percorrida: 300 km
+* Combustível gasto: 25 litros
+O programa deve calcular o consumo médio (km/l) e exibir todos os dados da viagem, 
+incluindo o resultado final arredondado para duas casas decimais.
+"""
+
+"""
+distancia_percorrida = 300  
+combustivel_gasto = 25      
+consumo_medio = distancia_percorrida / combustivel_gasto
+
+print("Distância percorrida: {} km".format(distancia_percorrida))
+print("Combustível gasto: {} litros".format(combustivel_gasto))
+print("Consumo médio: {:.2f} km/l".format(consumo_medio))
+
+"""
+
+distancia_percorrida = input("Digite a distância percorrida em km: ")
+combustivel_gasto = input("Digite a quantidade de combustível gasto em litros: ")
+distancia_percorrida = float(distancia_percorrida)
+combustivel_gasto = float(combustivel_gasto)
+consumo_medio = distancia_percorrida / combustivel_gasto
+
+print("Você percorreu: {} km".format(distancia_percorrida))
+print("A quantidade de combustível que você gastou foram: {} litros".format(combustivel_gasto))
+print("Então, o seu Consumo médio foi de: {:.2f} km/l".format(consumo_medio))
+
+
+
+
+
+
+
+
+#╔═══════════════════════════╗
+#║       </> vianassej       ║
+#╚═══════════════════════════╝
