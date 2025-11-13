33198376483bc9681cd7618145a302353f9f3a9b
Jessica Vylia <120236776+vianassej@users.noreply.github.com>
2025-11-12 21:01:34 -0300

    Questão 03

---
+++ atividades-12-11-25/questao-03.py
@@ -0,0 +1,49 @@
+"""
+3- Calculadora de Média Escolar
+Crie um programa que calcula a média escolar de um aluno. Use as seguintes notas:
+
+* Nota 1: 7.5
+* Nota 2: 8.0
+* Nota 3: 6.5
+O programa deve calcular a média e exibir todas as notas e o resultado final, 
+arredondando para duas casas decimais.
+
+"""
+
+"""
+nota1 = 7.5
+nota2 = 8.0
+nota3 = 6.5
+
+media = (nota1 + nota2 + nota3) / 3
+
+print("Nota 1: {:.2f}".format(nota1))
+print("Nota 2: {:.2f}".format(nota2))
+print("Nota 3: {:.2f}".format(nota3))
+print("Média final: {:.2f}".format(media))
+
+"""
+
+Aluno = input("Digite o nome do aluno: ")
+nota1 = input("Digite a primeira nota do(a)" + Aluno + ": ")
+nota2 = input("Digite a segunda nota do(a)" + Aluno + ": ")
+nota3 = input("Digite a terceira nota do(a)" + Aluno + ": ")
+
+media = (float(nota1) + float(nota2) + float(nota3)) / 3
+
+print("Nota 1: {:.2f}".format(float(nota1)))
+print("Nota 2: {:.2f}".format(float(nota2)))
+print("Nota 3: {:.2f}".format(float(nota3)))
+print("Média final: {:.2f}".format(media))
+
+
+print(f"A média final do(a) {Aluno} é {media:.2f}")
+
+
+
+
+
+
+#╔═══════════════════════════╗
+#║       </> vianassej       ║
+#╚═══════════════════════════╝
